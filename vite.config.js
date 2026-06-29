91fe39589c226ec951927b3285025822835de095
Tim Howard <tdhoward@gmail.com>
2026-06-28 19:15:53 -0700

    Cleaning up some naming stuff for WDS-2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       recompose: fileURLToPath(new URL('./src/recompose.js', import.meta.url)),
