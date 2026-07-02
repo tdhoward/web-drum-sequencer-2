d0bf0509621b2f9648839a51a73fc463f387f3dc
Tim Howard <tdhoward@gmail.com>
2026-07-01 17:07:39 -0700

    Bug fix for dragging channels

--- src/components/Knob.component.jsx
+++ src/components/Knob.component.jsx
@@ -26,29 +26,97 @@ export class Knob extends React.Component {
   componentDidMount() {
     const { onChange } = this.props;
     this.knob.addEventListener('input', onChange);
+    this.syncKnob();
   }
 
-  componentDidUpdate() {
-    const { value } = this.props;
-    this.knob.setValue(value);
+  componentDidUpdate(prevProps) {
+    const { onChange } = this.props;
+
+    if (prevProps.onChange !== onChange) {
+      this.knob.removeEventListener('input', prevProps.onChange);
+      this.knob.addEventListener('input', onChange);
+    }
+
+    this.syncKnob();
   }
 
-  render() {
+  componentWillUnmount() {
+    const { onChange } = this.props;
+
+    if (this.knob) {
+      this.knob.removeEventListener('input', onChange);
+    }
+  }
+
+  getKnobElementProps() {
     const { size, value, ...rest } = this.props;
     const theme = this.context || {};
     const knobTheme = theme.knob || {};
     const skin = knobSkins[knobTheme.skin] || knobSkins.default;
+
+    return {
+      src: skin.src,
+      sprites: skin.sprites || knobTheme.sprites || 50,
+      min: '0',
+      max: '100',
+      step: '1',
+      width: size,
+      height: size,
+      value,
+      ...rest,
+    };
+  }
+
+  syncKnob() {
+    if (!this.knob) {
+      return;
+    }
+
+    const {
+      src,
+      sprites,
+      min,
+      max,
+      step,
+      width,
+      height,
+      value,
+    } = this.getKnobElementProps();
+
+    [
+      ['src', src],
+      ['sprites', sprites],
+      ['min', min],
+      ['max', max],
+      ['step', step],
+      ['width', width],
+      ['height', height],
+      ['value', value],
+    ].forEach(([name, nextValue]) => {
+      if (nextValue === undefined) {
+        this.knob.removeAttribute(name);
+      } else {
+        this.knob.setAttribute(name, nextValue);
+      }
+    });
+
+    this.knob.src = src;
+    this.knob.sprites = sprites;
+    this.knob.min = min;
+    this.knob.max = max;
+    this.knob.step = step;
+    this.knob.width = width;
+    this.knob.height = height;
+    this.knob.setValue(value);
+  }
+
+  render() {
+    const knobProps = this.getKnobElementProps();
+
     return (
       <webaudio-knob
         ref={(element) => { this.knob = element; }}
-        src={skin.src}
-        sprites={skin.sprites || knobTheme.sprites || 50}
-        min="0"
-        max="100"
-        width={size}
-        height={size}
-        value={value}
-        {...rest}
+        {...knobProps}
       />
     );
   }
