4159a38e23c44136a2ad14c64c5c11a43d46ff44
Tim Howard <tdhoward@gmail.com>
2026-07-11 09:37:24 -0700

    Time sig playback bugfix

--- src/components/TimeSignatureSelector/TimeSignatureSelector.container.ts
+++ src/components/TimeSignatureSelector/TimeSignatureSelector.container.ts
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux';
 import {
+  playingSelector,
   selectedPatternIdSelector,
   selectedPatternStepsPerBeatSelector,
   selectedPatternTimeSignatureSelector,
   setPatternTimeSignature,
   type TimeSignaturePreset,
 } from '../../common';
+import { stopAllNotes } from '../../services/audioRouter';
+import { clearScheduledNotes } from '../../services/audioScheduler';
 import { TimeSignatureSelectorComponent } from './TimeSignatureSelector.component';
 import type { RootState } from '../../reducer';
 
 const mapStateToProps = (state: RootState) => ({
+  playing: playingSelector(state),
   selectedPatternId: selectedPatternIdSelector(state),
   selectedStepsPerBeat: selectedPatternStepsPerBeatSelector(state),
   selectedTimeSignature: selectedPatternTimeSignatureSelector(state),
@@ -26,6 +30,11 @@ const mergeProps = (
   selectedStepsPerBeat: stateProps.selectedStepsPerBeat,
   selectedTimeSignature: stateProps.selectedTimeSignature,
   onSelectTimeSignature: (preset: TimeSignaturePreset) => {
+    if (stateProps.playing) {
+      stopAllNotes();
+      clearScheduledNotes();
+    }
+
     dispatchProps.setPatternTimeSignature({
       patternId: stateProps.selectedPatternId,
       timeSignature: preset.timeSignature,
